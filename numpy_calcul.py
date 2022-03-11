9975154c18feea7df7df0ed14c226262a44e8195
Myunghyun_Ahn <ahn020367@gmail.com>
2022-03-11 09:38:24 +0900

    넘파이 배열 기본연산

---
+++ numpy_calcul.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+arr1 = np.arange(10)
+arr2 = np.arange(10, 20)
+
+print(arr1)
+
+print(arr2)
+
+print(arr1*2) #곱하기
+
+print(arr1/2) #나누기
+
+print(arr1+1) #더하기
+
+print(arr1**2) #제곱
+ 
+print(arr1) #기존의 값은 변화 없음
+
+print(arr1 + arr2) #배열끼리 더하기
+
+print(arr1 * arr2) #배열끼리 곱하기
+
+print(arr1 / arr2) #배열끼리 나누기
