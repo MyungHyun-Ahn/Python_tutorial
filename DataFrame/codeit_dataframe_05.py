f380b14658518dee12c255bafddf2f27fc633696
Myunghyun_Ahn <ahn020367@gmail.com>
2022-03-14 09:29:24 +0900

    코드잇 데이터프레임 실전문제 05

---
+++ DataFrame/codeit_dataframe_05.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+samsong_df = pd.read_csv('data/samsong.csv')
+hyundee_df = pd.read_csv('data/hyundee.csv')
+
+# 코드를 작성하세요.
+dict_m = {
+    'day' : samsong_df['요일'],
+    'samsong' : samsong_df['문화생활비'],
+    'hyundee' : hyundee_df['문화생활비']
+}
+
+df = pd.DataFrame(dict_m)
+
+df
