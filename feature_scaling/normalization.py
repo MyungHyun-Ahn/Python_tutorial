e32427d156d4893eabe181519e5cd4d8cc887652
Myunghyun_Ahn <ahn020367@gmail.com>
2022-03-18 14:15:56 +0900

    Normalization

---
+++ feature_scaling/normalization.py
@@ -0,0 +1,31 @@
+from locale import normalize
+import pandas as pd
+import numpy as np
+from pygments import highlight
+
+from sklearn import preprocessing
+
+NBA_FILE_PATH = 'feature_scaling/data/NBA_player_of_the_week.csv'
+
+nba_player_of_the_week = pd.read_csv(NBA_FILE_PATH)
+
+nba_player_of_the_week.head()
+
+nba_player_of_the_week.describe()
+
+# cm / kg feature scaling
+
+height_weight_age_df = nba_player_of_the_week[['Height CM', 'Weight KG', 'Age']]
+
+height_weight_age_df.head()
+
+# min-max normalization
+
+# 0 - 1 로 바꾸어주는 도구
+scaler = preprocessing.MinMaxScaler()
+normalized_data = scaler.fit_transform(height_weight_age_df)
+
+normalized_df = pd.DataFrame(normalized_data, columns=['Height', 'Weight', 'Age'])
+
+normalized_df.head()
+
