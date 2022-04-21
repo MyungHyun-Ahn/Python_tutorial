b4397ac37edb61b33ee3b2a829a0153bbde369d6
Myunghyun_Ahn <ahn020367@gmail.com>
2022-04-21 10:07:03 +0900

    객체지향 다형성

--- objected_oriented/Polymorphism.py
+++ objected_oriented/Polymorphism.py
@@ -71,17 +71,32 @@ class Paint:
     def __init__(self):
         self.shapes = []
 
-    def add_shape(self, shape):
-        """그림판에 도형을 추가한다"""
+    def add_shape(self, shape: Shape):
+        """그림판에 도형 인스턴스 shape를 추가한다. 단, shape는 추상 클래스 Shape의 인스턴스여야 한다."""
         # shape 가 Shape 클래스의 인스턴스라면
+        """
+        # LBYL 스타일
         if isinstance(shape, Shape):
             self.shapes.append(shape)
         else:
             print("넓이, 둘레를 구하는 메소드가 없는 도형은 추가할 수 없습니다.")
+        """
+        # EAFP 스타일
+        self.shapes.append(shape)
 
     def total_area_of_shapes(self):
         """그림판에 있는 모든 도형의 넓이의 합을 구한다"""
+        """
         return sum([shape.area() for shape in self.shapes])
+        """
+        total_area = 0
+
+        for shape in self.shapes:
+            try:
+                total_area+=shape.area()
+            except (AttributeError, TypeError):
+                print("그림판에 area 메소드가 없거나 잘못 정의되어 있는 인스턴스 {}가 있습니다.".format(shape))
+            return total_area
 
     def total_perimeter_of_shapes(self):
         """그림판에 있는 모든 도형의 둘레의 합을 구한다"""
@@ -100,6 +115,7 @@ circle = Circle(4)
 paint_program = Paint()
 paint_program.add_shape(rectangle)
 paint_program.add_shape(circle)
+paint_program.add_shape(1)
 
 print(paint_program.total_area_of_shapes())
-print(paint_program.total_perimeter_of_shapes())
+#print(paint_program.total_perimeter_of_shapes())
